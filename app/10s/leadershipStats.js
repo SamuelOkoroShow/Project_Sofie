63841b7ca264cd916da095ef71514cb2a55d2ed7
Sam Okoro <s.okoro07@gmail.com>
2017-09-23 18:32:14 -0500

    Index Routing

---
+++ app/10s/leadershipStats.js
@@ -0,0 +1,3 @@
+var leader = []
+
+export default leader;
---
+++ app/ui/widgets/eachCard.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export class eachCard extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
